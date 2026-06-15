7321cdf7f4833a9a7d52b238be0f7d06337004e1
Vedang07-eng <dhurivedang4104@gmail.com>
2026-06-15 17:42:20 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://datastraw-assignment-1.onrender.com",
+    "baseURL: "https://datastraw-assignment-1.onrender.com/api"",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
